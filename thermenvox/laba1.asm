df3d94e8e7ff8a0e0fc276fe9bf6cceaf86aa8bb
marina1177 <marina1177@gmail.com>
2019-10-12 23:53:43 +0300

    evrika

--- thermenvox/laba1.asm
+++ thermenvox/laba1.asm
@@ -1,16 +1,14 @@
 .include "m8515def.inc"
 
 
-.def hex = r16     ;  ���� 8-������� �����
-.def n = r17
+.def chn = r17
 .def cnt = r18
 .def t1 = r19
 
-.equ  convst= 0	    ; 
-.equ  busy =  1		; 
-.equ  miso  = 6  	; 
-.equ  sck =   7		;
-
+.equ  ncs =		4	    ; 
+.equ  selch =	0		; 
+.equ  miso =	6  	; 
+.equ  clk =		7		;
 
 
 rjmp main
@@ -20,23 +18,42 @@ rjmp SPI_
 
 
 main:
-
+	cli
 	init_stek:
 		ldi R16,LOW(RamEnd)
 		out SPL,R16
 		ldi R16,HIGH(RamEnd)
 		out SPH,R16
 	init_spi:
+		ldi   t1,0b10011111	   
+	    out   DDRB,t1	    ; PB0-control channel,PB4-NCS,PB6-MISO, PB5-MOSI,PB7-SCK
 	    ldi   t1,0b00110011       
 	    out   PORTB,t1      ;  
-	    ldi   t1,0b10000001	   
-	    out   DDRB,t1	    ; PB0-convst,PB1-busy,PB6-MISO,PB7-SCK
+	    
+	start:
+		ldi t1,0b11010000; SPIE,SPE,
+		out		SPCR,	t1; interrapt enable	
+
+		cbi		PORTB, ncs	;cs = 0
+		sbi		PORTB, chn	;DI(start bit) = 1
+		sbi		PORTB, clk	;UP CLK
+		sbic 	PORTB, 0	;select channel(SGL/DIF)
+		nop					;CH0
+		cbi		PORTB, chn	;CH1
+		cbi		PORTB, clk	;DOWN CLK
+		sbic 	PORTB, 0	;select channel(ODD/SIGN)
+		cbi		PORTB, 0	;CH0
+		nop					;CH1
+
+
+
+
 
 
 read_adc:	;
 		
 	ldi   cnt,8		;
-	cbi   PortB,convst  ;
+	sbi   PortB, ncs  ;cs
 	nop
 	sbi   PortB,convst
 	Busy1:
