b494f976f58a30df60b70bb411df8f683b5a456b
marina1177 <marina1177@gmail.com>
2019-10-17 07:45:54 +0300

    last

--- thermenvox/laba1.asm
+++ thermenvox/laba1.asm
@@ -1,9 +1,10 @@
 .include "m8515def.inc"
 
-
+.def flgs = r16;
 .def chn = r17
-.def cnt = r18
-.def t1 = r19
+.def data_ch0 = r18
+.def data_ch1 = r19
+.def n = r20
 
 .equ  ncs =		4	    ; 
 .equ  selch =	0		; 
@@ -14,7 +15,7 @@
 rjmp main
 
 .org 0x0008
-rjmp SPI_
+rjmp SPI_interrapt
 
 
 main:
@@ -25,50 +26,90 @@ main:
 		ldi R16,HIGH(RamEnd)
 		out SPH,R16
 	init_spi:
-		ldi   t1,0b10011111	   
-	    out   DDRB,t1	    ; PB0-control channel,PB4-NCS,PB6-MISO, PB5-MOSI,PB7-SCK
-	    ldi   t1,0b00110011       
-	    out   PORTB,t1      ;  
-	    
-	start:
-		ldi t1,0b11010000; SPIE,SPE,
-		out		SPCR,	t1; interrapt enable	
+		ldi   R16,0b10011111	   
+	    out   DDRB,R16	    ; PB0-control channel,PB4-NCS,PB6-MISO, PB5-MOSI,PB7-SCK
+	    ldi   R16,0b00110011       
+	    out   PORTB,R16     ;  
 
-		cbi		PORTB, ncs	;cs = 0
-		sbi		PORTB, chn	;DI(start bit) = 1
-		sbi		PORTB, clk	;UP CLK
+		ldi R16,0b11010100; CPHA,SPIE,SPE,MSTR
+		out		SPCR,	R16; interrapt enable	
+		sei
+	
+		cli flgs; f0 - ch0/1, f1 - channel count
+
+	select_channel:
+		cpi		flgs,	2
+		breq	transfer_data ;both registers are full
+		rjmp	rvrs_ch			
+			
+
+	start_spi:
+
+		cbi		PORTB,	ncs	;cs = 0
+		sbi		PORTB,	chn	;DI(start bit) = 1
+		
+		sbi		PORTB,	clk	;UP CLK-1
+		cbi		PORTB,	clk	;DOWN CLK-1
+	
 		sbic 	PORTB, 0	;select channel(SGL/DIF)
 		nop					;CH0
 		cbi		PORTB, chn	;CH1
-		cbi		PORTB, clk	;DOWN CLK
+
+		sbi		PORTB, clk	;UP CLK-2
+		cbi		PORTB, clk	;DOWN CLK-2
+
 		sbic 	PORTB, 0	;select channel(ODD/SIGN)
 		cbi		PORTB, 0	;CH0
 		nop					;CH1
 
+		sbi		PORTB, clk	;UP CLK-3
+		cbi		PORTB, clk	;DOWN CLK-3
 
-
-
-
-
-read_adc:	;
-		
-	ldi   cnt,8		;
-	sbi   PortB, ncs  ;cs
-	nop
-	sbi   PortB,convst
-	Busy1:
-		sbic  PINB,busy     ;
-		rjmp  Busy1
 	spi_loop:
-		lsl   hexL
-		rol   hexH
-		sbi   PortB,sck
-		cbi   PortB,sck
-		sbic  PINB,miso
-		inc hexL
-		dec   Cnt
-		brne  spi_loop
-ret		
+
+		sbi		PORTB,	clk	;UP CLK
+		cbi		PORTB,	clk	;DOWN CLK
+
+		sbrc	flgs,	0	;пропуск если flgs0 = 0
+		rjmp	start_spi	;
+		rjmp	spi_loop				
+
+		ret
+
+
+
+
+SPI_interrapt:
+	
+	sbi		PORTB,	ncs	;cs = 1
+
+	sbic	PORTB0,	0	
+	rjmp	solve_0
+
+	sbis	PORTB0, 0
+	rjmp	solve_1	
+
+	solve_0:
+		
+		inc		flgs;если выбран ch0 -> flgs = 1;
+		in		data_ch0, SPDR
+		rjmp	next	
+
+	solve_1:
+		
+		inc		flgs		;если выбран ch1 -> flgs = 2;
+		in		data_ch1, SPDR
+		rjmp	next	
+	
+	next:
+		reti
+	
+		
+
+
+
+
+
 	
 		
 		
